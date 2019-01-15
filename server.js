63b8df5b43b2bdcc0e2da501b62e7313fd51a5bf
Burhan <bharmal.burhan@hotmail.com>
2019-01-15 18:21:28 +0530

    - add build command in package.json file - add server.js file - add .babelrc file for babel to compile es6 to es5

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from 'express'
+
+const app = express()
+
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  return res.status(200).send({'message': 'YAY! Congratulations! Your first endpoint is working'});
+})
+
+app.listen(3000)
+console.log('app running on port ', 3000)
