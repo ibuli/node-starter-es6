264d42657ff903e944ccc0829af01602428e48a7
Burhan <bharmal.burhan@hotmail.com>
2019-01-15 18:26:36 +0530

    - add babel-watch to build continuous changes on server.js file - add command to build in package.json file

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const app = express()
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  return res.status(200).send({'message': 'YAY! Congratulations! Your first endpoint is working'});
+  return res.status(200).send({'message': 'YAY! Congratulations! Your first endpoint is working'})
 })
 
 app.listen(3000)
